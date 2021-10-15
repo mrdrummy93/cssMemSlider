9d5c0fb601d4bc99340e56b1c7fd797d3e7da796
mrdrummy93 <vasya_2393@drummer.ru>
2021-10-16 02:19:05 +0300

    feat: start adding script (SAT, Oct 16, 2021 02:18 AM)

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
+const slides = document.querySelectorAll('.app-wrapper .slide');
+let currentSlide = 0;
+let isEnabled = true;
 
+function changeCurrentSlide(s) {
+  currentSlide = (s + slides.length) % slides.length;
+}
+
+function hideSlide(direction) {
+  isEnabled = false;
+  slides[currentSlide].classList.add(direction);
+  slides[currentSlide].addEventListener('animationend', function() {
+    this.classList.remove('active', direction)
+  })
+}
+
+function showSlide(direction) {
+  slides[currentSlide].classList.add('next', direction);
+  slides[currentSlide].addEventListener('animationend', function() {
+    this.classList.remove('next', direction);
+    this.classList.add('active');
+    isEnabled = true;
+  })
+}
 
