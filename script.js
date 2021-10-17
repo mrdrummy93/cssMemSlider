1cb490ff5d48396eb5976c6c77165a2d35baf735
mrdrummy93 <vasya_2393@drummer.ru>
2021-10-17 15:26:11 +0300

    feat: finish adding js

--- script.js
+++ script.js
@@ -1,25 +1,69 @@
-const slides = document.querySelectorAll('.app-wrapper .slide');
-let currentSlide = 0;
-let isEnabled = true;
+const slides = document.querySelectorAll('.slide'),
+  dots = document.querySelectorAll('.dot'),
+  descs = document.querySelectorAll('.desc');
 
-function changeCurrentSlide(s) {
-  currentSlide = (s + slides.length) % slides.length;
+let index = 0;
+
+function activeSlide(s) {
+  for (let slide of slides) {
+    slide.classList.remove('active');
+  }
+  slides[s].classList.add('active');
+}
+
+function activeDot(d) {
+  for ( let dot of dots) {
+    dot.classList.remove('active');
+  }
+  dots[d].classList.add('active');
+}
+
+function activeDesc(d) {
+  for (let desc of descs) {
+    desc.classList.remove('active');
+  }
+  descs[d].classList.add('active');
 }
 
 function hideSlide(direction) {
-  isEnabled = false;
-  slides[currentSlide].classList.add(direction);
-  slides[currentSlide].addEventListener('animationend', function() {
+  slides[index].classList.add(direction);
+  slides[index].addEventListener('animationend', function() {
     this.classList.remove('active', direction)
   })
 }
 
 function showSlide(direction) {
-  slides[currentSlide].classList.add('next', direction);
-  slides[currentSlide].addEventListener('animationend', function() {
+  slides[index].classList.add('next', direction);
+  slides[index].addEventListener('animationend', function() {
     this.classList.remove('next', direction);
     this.classList.add('active');
-    isEnabled = true;
   })
 }
 
+function hideDesc(direction) {
+  descs[index].classList.add(direction);
+  descs[index].addEventListener('animationend', function() {
+    this.classList.remove('active', direction)
+  })
+}
+
+function showDesc(direction) {
+  descs[index].classList.add('next', direction);
+  descs[index].addEventListener('animationend', function() {
+    this.classList.remove('next', direction);
+    this.classList.add('active');
+  })
+}
+
+function prepareCurrentSlide(ind) {
+  activeSlide(ind);
+  activeDot(ind);
+  activeDesc(ind);
+}
+
+dots.forEach((dot, indexDot) => {
+  dot.addEventListener('click', () => {
+    index = indexDot;
+    prepareCurrentSlide(index);
+  });
+});
